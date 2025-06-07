1592d7a18cd6bed06bfab47b5328f49644be121e
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-06-07 20:50:41 -0300

    Added new class TrafficReading

--- roads/models.py
+++ roads/models.py
@@ -7,4 +7,11 @@ class RoadSegment(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+class TrafficReading(models.Model):
+    Road_Segment = models.ForeignKey(RoadSegment, on_delete=models.CASCADE, related_name='readings')
+    average_speed = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.Road_Segment.name} - {self.average_speed} km/h'
