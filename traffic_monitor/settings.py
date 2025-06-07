2a99eb976b10b3dfd8d6c5152e1c0f883b23f8c7
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-06-07 20:01:40 -0300

    Fixed Settings.py bug

--- traffic_monitor/settings.py
+++ traffic_monitor/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'roads'
-    'rest_framework'
+    'roads',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
