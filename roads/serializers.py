59da42ef4bd8280110a93c12c9a304aa6f993987
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-06-08 20:12:44 -0300

    Added get_intensity() method

--- roads/serializers.py
+++ roads/serializers.py
@@ -10,3 +10,13 @@ class TrafficReadingSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrafficReading
         fields = '__all__'
+
+    def get_intensity(self, obj):
+        speed = obj.average_speed
+
+        if speed <= 20:
+            return 'elevada'
+        elif speed <= 50:
+            return 'média'
+        return 'baixa'
+    
