b062de514a6c8891447a5ae2beec3ada13ad8436
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-06-07 22:09:00 -0300

    Added serializer, urls, and views

--- roads/urls.py
+++ roads/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RoadSegmentList
+from .views import RoadSegmentList, TrafficReadingListCreate
 
 urlpatterns = [
-    path('roadsegments/', RoadSegmentList.as_view(), name='roadsegment-list')
+    path('roadsegments/', RoadSegmentList.as_view(), name='roadsegment-list'),
+    path('readings/', TrafficReadingListCreate.as_view(), name='reading-list')
 ]
