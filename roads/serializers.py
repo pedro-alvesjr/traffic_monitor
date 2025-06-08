7220caed9c0ca2a4d3b788525211c127f601c98a
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-06-07 21:44:53 -0300

    Update serializers.py

--- roads/serializers.py
+++ roads/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
-from .models import RoadSegment
+from .models import RoadSegment, TrafficReading
 
 class RoadSegmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = RoadSegment
         fields = '__all__'
-        
+
+class TrafficReadingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrafficReading
+        fields = '__all__'
